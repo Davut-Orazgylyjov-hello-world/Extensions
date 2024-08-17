32935ec3043c2f86af56c867dae6e8f127760728
Davut-Orazgylyjov-hello-world <88382528+Davut-Orazgylyjov-hello-world@users.noreply.github.com>
2024-08-17 10:53:58 +0300

    added enum extension for get random value

--- Scripts/Enum.cs
+++ Scripts/Enum.cs
@@ -1,17 +1,19 @@
 using System;
+using Random = UnityEngine.Random;
 
 namespace Extensions
 {
     public static class Enum
     {
-        // public static T Next<T>(this T src) where T : struct
-        // {
-        //     if (!typeof(T).IsEnum)
-        //         throw new ArgumentException(String.Format("Argument {0} is not an Enum", typeof(T).FullName));
-        //
-        //     T[] Arr = (T[])Enum.GetValues(src.GetType());
-        //     int j = Array.IndexOf<T>(Arr, src) + 1;
-        //     return (Arr.Length == j) ? Arr[0] : Arr[j];
-        // }
+        public static T RandomEnum<T>(this T en) where T : struct, IConvertible
+        {
+            if (!typeof(T).IsEnum)
+            {
+                throw new Exception("random enum variable is not an enum");
+            }
+            
+            var values = System.Enum.GetValues(typeof(T));
+            return (T) values.GetValue(Random.Range(0, values.Length));
+        }
     }
 }
